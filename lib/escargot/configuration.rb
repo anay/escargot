0ca0426899dec41915758936e3811ab0e226cbb1
Patrick Tulskie <patricktulskie@gmail.com>
2013-05-07 10:31:21 -0400

    Updated configuration file to better determine environment and allow for setting app_root.

--- lib/escargot/configuration.rb
+++ lib/escargot/configuration.rb
@@ -3,27 +3,40 @@ module Escargot
     class << self
       
       def env
-        Rails.env
+        if defined?(Rails)
+          Rails.env
+        elsif ENV['RACK_ENV']
+          ENV['RACK_ENV']
+        elsif defined?(Sinatra::Application)
+          Sinatra::Application.environment.to_s
+        end
       end
     
       def settings
-        user_configuration || {"host" => 'localhost', "port" => '9200', "timeout" => 20}
+        user_configuration || { "host" => 'localhost', "port" => '9200', "timeout" => 20 }
       end
     
       def user_configuration
-        path = File.join(app_root, "/config/elasticsearch.yml")
+        path = File.join(app_root, "config", "elasticsearch.yml")
         if File.exists?(path) && env_hash = YAML.load(File.open(path, 'r').read)[env]
           env_hash
         else
           nil
         end
       end
+      
+      def app_root=(root_path)
+        @app_root = root_path
+      end
     
       def app_root
-        if defined?(Rails.root)
+        @app_root ||= case 
+        when defined?(Rails)
           Rails.root
-        elsif defined?(Sinatra)
+        when defined?(Sinatra)
           Sinatra::Application.root
+        else
+          Dir.pwd
         end
       end
       
